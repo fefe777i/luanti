c4595b3f73b4c01fc849adec9ef9069e12ae6293
fefe777i <artomlukasuk9@gmail.com>
2026-07-04 19:19:12 +0200

    Remove Content and About tabs from main menu

--- builtin/mainmenu/init.lua
+++ builtin/mainmenu/init.lua
@@ -41,8 +41,6 @@ dofile(menupath .. DIR_DELIM .. "dlg_clients_list.lua")
 dofile(menupath .. DIR_DELIM .. "dlg_server_list_mods.lua")
 
 local tabs = {
-	content  = dofile(menupath .. DIR_DELIM .. "tab_content.lua"),
-	about = dofile(menupath .. DIR_DELIM .. "tab_about.lua"),
 	local_game = dofile(menupath .. DIR_DELIM .. "tab_local.lua"),
 	play_online = dofile(menupath .. DIR_DELIM .. "tab_online.lua")
 }
@@ -112,8 +110,6 @@ local function init_globals()
 	tv_main:set_autosave_tab(true)
 	tv_main:add(tabs.local_game)
 	tv_main:add(tabs.play_online)
-	tv_main:add(tabs.content)
-	tv_main:add(tabs.about)
 
 	tv_main:set_global_event_handler(main_event_handler)
 	tv_main:set_fixed_size(false)
