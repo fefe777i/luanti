ef2506e7e874697a292d710ec40dccb9024751b8
fefe777i <artomlukasuk9@gmail.com>
2026-08-10 19:26:27 +0200

    Add skin selector menu + fix c_content conflict

--- builtin/mainmenu/init.lua
+++ builtin/mainmenu/init.lua
@@ -41,6 +41,7 @@ dofile(menupath .. DIR_DELIM .. "dlg_rebind_keys.lua")
 dofile(menupath .. DIR_DELIM .. "dlg_clients_list.lua")
 dofile(menupath .. DIR_DELIM .. "dlg_server_list_mods.lua")
 dofile(menupath .. DIR_DELIM .. "dlg_skin_selector.lua")
+dofile(menupath .. DIR_DELIM .. "dlg_skin_selector.lua")
 
 local tabs = {
 	local_game = dofile(menupath .. DIR_DELIM .. "tab_local.lua"),
