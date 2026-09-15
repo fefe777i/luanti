3d5d85a24c856e12348eef2d6a3b9c3dac4d9dcd
fefe777i <artomlukasuk9@gmail.com>
2026-09-15 20:34:04 +0300

    Add Multiworld player subworld state header

---
+++ src/subworld.h
@@ -0,0 +1,12 @@
+// Luanti
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+#pragma once
+
+#include <string>
+#include <unordered_map>
+
+struct PlayerSubWorldState {
+	std::string current_subworld = "overworld";
+	std::unordered_map<std::string, v3f> positions;
+};
