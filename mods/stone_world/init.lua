f6d2d781299539b4ee971a5b05b3af5db652cb60
fefe777i <artomlukasuk9@gmail.com>
2026-09-18 22:10:30 +0300

    Use real world reconnect switching

--- mods/stone_world/init.lua
+++ mods/stone_world/init.lua
@@ -1,113 +1,54 @@
-local MOD = minetest.get_current_modname()
-local STORAGE = minetest.get_mod_storage()
-local WORLD_NAME = "stone_world"
-local SPAWN = vector.new(0, 20, 0)
-local ITEM = MOD .. ":teleporter"
-local WORLD_KEY = "world"
+local WORLD_1 = "world1"
+local WORLD_2 = "world2"
 
-local function move_player(player, world_name, pos)
-	local name = player:get_player_name()
-
-	if not minetest.transfer_player then
+local function switch_world(name)
+	if not minetest.switch_world then
 		return false
 	end
-
-	if not minetest.transfer_player(name, world_name, pos) then
-		return false
-	end
-
-	STORAGE:set_string(WORLD_KEY .. ":" .. name, world_name)
-	return true
+	return minetest.switch_world(name)
 end
 
-local function enter_stone_world(player)
-	if not player or not player:is_player() then
-		return false
-	end
+minetest.register_chatcommand("world", {
+	params = "<name>",
+	description = "Перемкнутися в інший світ",
+	privs = {},
 
-	if not minetest.create_subworld or not minetest.create_subworld(WORLD_NAME) then
-		return false
-	end
+	func = function(_, param)
+		param = param:gsub("^%s+", ""):gsub("%s+$", "")
+		if param == "" then
+			return false, "Вкажи назву світу: /world world1"
+		end
 
-	return move_player(player, WORLD_NAME, SPAWN)
-end
+		if not switch_world(param) then
+			return false, "Не вдалося перемкнути світ"
+		end
 
-local function leave_stone_world(player)
-	if not player or not player:is_player() then
-		return false
-	end
-
-	return move_player(player, "overworld", SPAWN)
-end
-
-minetest.register_tool(ITEM, {
-	description = "Кам'яний світ",
-	inventory_image = "default_stone.png",
-	stack_max = 1,
-
-	on_use = function(itemstack, user)
-		enter_stone_world(user)
-		return itemstack
-	end,
-
-	on_place = function(itemstack, user)
-		enter_stone_world(user)
-		return itemstack
+		return true, "Перепідключення до світу " .. param .. "..."
 	end,
 })
 
-minetest.register_on_joinplayer(function(player)
-	local inv = player:get_inventory()
-	if not inv:contains_item("main", ITEM) then
-		inv:add_item("main", ITEM)
-	end
-
-	local name = player:get_player_name()
-	local world = STORAGE:get_string(WORLD_KEY .. ":" .. name)
-
-	if world == WORLD_NAME and minetest.transfer_player then
-		minetest.after(0.2, function()
-			if player:is_player() then
-				minetest.transfer_player(name, WORLD_NAME, vector.new(0, 22, 0))
-			end
-		end)
-	end
-end)
-
 minetest.register_chatcommand("stoneworld", {
 	params = "",
-	description = "Телепортуватися у Кам'яний світ",
+	description = "Перейти у world2",
 	privs = {},
 
-	func = function(name)
-		local player = minetest.get_player_by_name(name)
-		if not player then
-			return false, "Гравця не знайдено"
+	func = function()
+		if not switch_world(WORLD_2) then
+			return false, "Не вдалося перейти у world2"
 		end
-
-		if not enter_stone_world(player) then
-			return false, "Не вдалося перейти у Кам'яний світ"
-		end
-
-		return true, "Ти у Кам'яному світі"
+		return true, "Перепідключення до world2..."
 	end,
 })
 
 minetest.register_chatcommand("overworld", {
 	params = "",
-	description = "Повернутися з Кам'яного світу",
+	description = "Повернутися у world1",
 	privs = {},
 
-	func = function(name)
-		local player = minetest.get_player_by_name(name)
-		if not player then
-			return false, "Гравця не знайдено"
+	func = function()
+		if not switch_world(WORLD_1) then
+			return false, "Не вдалося перейти у world1"
 		end
-
-		if not leave_stone_world(player) then
-			return false, "Не вдалося повернутися у звичайний світ"
-		end
-
-		return true, "Ти повернувся у звичайний світ"
+		return true, "Перепідключення до world1..."
 	end,
 })
