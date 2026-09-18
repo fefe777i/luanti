379900775ae48d49fbb5ea0206d2c5d9fb3f8d29
fefe777i <artomlukasuk9@gmail.com>
2026-09-18 21:34:24 +0300

    Make obsolete Multiworld transfer patch idempotent

--- apply_multiworld_transfer.sh
+++ apply_multiworld_transfer.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -e
-sed -i '/if (subworld_name != "overworld" &&/a\	if (subworld_name != "overworld") pos.X += 12500 * MAP_BLOCKSIZE;' src/server.cpp
+echo "Multiworld transfer patch is already implemented in src/server.cpp"
