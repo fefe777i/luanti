0b2fca861bbbe382fab2437882b8f0f4ba699c61
fefe777i <artomlukasuk9@gmail.com>
2026-09-19 09:41:46 +0300

    multiworld: add world instance registry

--- src/server.h
+++ src/server.h
@@ -28,7 +28,7 @@
 #include <optional>
 #include <string_view>
 #include <shared_mutex>
-#include <condition_variable>
+#include <condition_variable>\n#include <memory>
 
 class BanManager;
 class ChatEvent;
@@ -470,7 +470,7 @@ public:
 	// Identical but for mapgen env
 	std::vector<std::pair<std::string, std::string>> m_mapgen_init_files;
 
-	std::unordered_map<std::string, PlayerSubWorldState> m_player_subworld_states;
+	std::unordered_map<std::string, PlayerSubWorldState> m_player_subworld_states;\n\n\t// Registry of physical world instances. Each entry represents a separate\n\t// world directory; player routing will select the instance without using\n\t// coordinate offsets.\n\tstd::unordered_map<std::string, std::unique_ptr<WorldInstance>> m_world_instances;
 
 	// Data transferred into other Lua envs at init time
 	std::unique_ptr<PackedValue> m_lua_globals_data;
